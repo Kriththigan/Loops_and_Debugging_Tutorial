f8b10e32f21935acda1db910c9ca8222b1e32593
Kriththigan <kriththiganklp10@gmail.com>
2025-11-18 20:01:44 +0530

    commit and push

---
+++ src/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args){
+        int sum = 0;
+        int i = 1;
+        while (i <=10){
+            sum += i;
+            i++;
+        }
+        System.out.println("The sum of integers 1 to 10 is:" + sum);
+    }
+}
